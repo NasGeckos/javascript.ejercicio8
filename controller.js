ab63dfed53c9889edf2501c33676c7bec7f201df
NasGeckos <110292428+NasGeckos@users.noreply.github.com>
2022-10-12 13:41:18 +0200

    Update and rename matematicas.js to controller.js

---
+++ controller.js
@@ -0,0 +1,8 @@
+ // modulo, parte de codigo que se pede reutilizar en diferentes partes del programa
+export function suma(a, b){
+    return a + b;
+}
+
+export function multiplica(a, b){
+    return a * b;
+}

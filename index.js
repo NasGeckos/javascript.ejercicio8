5aaea1a5c8928a43a5c59e09916bd9cc7a9a0c1a
NasGeckos <110292428+NasGeckos@users.noreply.github.com>
2022-10-12 13:44:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import * as moduloMatematicas from './modulos/controller.js' // AQUI IMPORTAMOS TODO
 
-const sum = moduloMatematicas.suma(4, 12)
+const sum = moduloMatematicas.suma(1, 2)
 console.log(sum)
 
-const multiplicacion = moduloMatematicas.multiplica(2, 21)
+const multiplicacion = moduloMatematicas.multiplica(4, 5)
 console.log(multiplicacion)
 
-console.log(getAuthor())
